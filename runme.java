03720880fd1fcdad5c4dd37cddbe4d2dd1b44427
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-09-21 21:40:11 +0530

    ...

---
+++ runme.java
@@ -0,0 +1,11 @@
+// runme.java
+
+public class runme {
+  static {
+   System.loadLibrary("example");
+  }
+
+  public static void main(String argv[]) {
+    System.out.println(example.fact(4));
+  }
+}
