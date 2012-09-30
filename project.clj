48ae47cf3142d710cef9a41e240fbba8d46316b6
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-09-30 15:03:48 +0530

    ...

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject one "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
+  :java-source-paths ["gen_java_src" "src/java"]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [shake "0.2.1"]])
 
