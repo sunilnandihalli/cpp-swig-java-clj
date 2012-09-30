56a8c723955cb301d776bc53bce09bf01ff18fd5
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-09-30 20:15:35 +0530

    ...

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :java-source-paths ["gen_java_src" "src/java"]
   :native-path "cpp_lib"
+  :jvm-opts [~(str "-Djava.library.path=cpp_lib/:" (System/getenv "LD_LIBRARY_PATH"))]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [shake "0.2.1"]])
 
--- src/clj/one/core.clj
+++ src/clj/one/core.clj
@@ -2,9 +2,18 @@
   (:require [clojure.pprint :as p])
   (:import complex complexDouble complexInt vecInt
            misc_utils))
+(comment
+  (let [p (System/getProperties)
+        keys (.keys p)]
+    (loop []
+      (if (.hasMoreElements keys)
+        (let [key (.nextElement keys)
+              value (.get p key)]
+          (p/pprint {:key key :value value})
+          (recur))))))
 
 
-(System/loadLibrary "libwrapper.so")
+(System/loadLibrary "wrapper")
 
 (let [x (doto (complex. 10.0 20.0)
           (.setRe 10.0)
