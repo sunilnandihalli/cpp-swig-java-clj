1c6263bf756f769e383386fc9109c21a988935ec
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-10-01 06:41:16 +0530

    ....

--- src/cpp/stuff.cpp
+++ src/cpp/stuff.cpp
@@ -1,15 +1,16 @@
 #include "stuff.h"
-
-int fact(int n) {
-  return n<=1?1:n*fact(n-1);
-}
-
-complex::complex(double _re,double _im):re(_re),im(_im) {
-}
-complex complex::operator+(const complex& b) {
-  return complex(re+b.re,im+b.im);
-}
-
-complex complex::add(const complex& a) {
-  return complex(re+a.re,im+a.im);
+namespace misc_utils {
+  int fact(int n) {
+    return n<=1?1:n*fact(n-1);
+  }
+  
+  complex::complex(double _re,double _im):re(_re),im(_im) {
+  }
+  complex complex::operator+(const complex& b) {
+    return complex(re+b.re,im+b.im);
+  }
+  
+  complex complex::add(const complex& a) {
+    return complex(re+a.re,im+a.im);
+  }
 }
