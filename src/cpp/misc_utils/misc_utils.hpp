ea747899f9b3bed0bbd7c79d6d94e8ab7642a803
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-10-02 13:21:49 +0530

    ...

---
+++ src/cpp/misc_utils/misc_utils.hpp
@@ -0,0 +1,26 @@
+#ifndef MISC_UTILS_HPP
+#define MISC_UTILS_HPP
+namespace misc_utils {
+  int fact(int n);
+  class complex {
+  public:
+    double re,im;
+    complex(double _re,double _im);
+    complex operator+(const complex& b);
+    complex add(const complex& a);
+  };
+  
+  template <class T>
+  struct complexT {
+    T re,im;
+    complexT(T _re,T _im):re(_re),im(_im) {
+    }
+    complexT<T> operator+(const complexT<T>& a) {
+      return complexT<T>(re+a.re,im+a.im);
+    }
+    complexT<T> add(const complexT<T>& a) {
+      return complexT<T>(re+a.re,im+a.im);
+    }
+  };
+}
+#endif
