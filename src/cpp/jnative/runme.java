19d73e658dfb39b1d76f7eacb646a38f3b3ba75a
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-10-21 09:53:22 +0530

    ...-a

---
+++ src/cpp/jnative/runme.java
@@ -0,0 +1,19 @@
+
+public class runme {
+
+  static {
+    try {
+	System.loadLibrary("jnative_java");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println("Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" + e);
+      System.exit(1);
+    }
+  }
+
+  public static void main(String argv[]) {
+    SWIGTYPE_p_Point p = jnative_module.point_create(1, 2);
+    System.out.println("auto wrapped  : " + jnative_module.point_toString1(p));
+    System.out.println("manual wrapped: " + jnative_module.point_toString2(p));
+    jnative_module.free(new SWIGTYPE_p_void(SWIGTYPE_p_Point.getCPtr(p), false)); //clean up c allocated memory
+  }
+}
