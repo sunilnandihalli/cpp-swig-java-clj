1b1b42902febcb7f2197edcc467725df353f5711
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-10-21 09:56:42 +0530

    everything compiles....

--- src/cpp/multimap/runme.java
+++ src/cpp/multimap/runme.java
@@ -15,19 +15,19 @@ public class runme {
     // Call our gcd() function
     int x = 42;
     int y = 105;
-    int g = example.gcd(x,y);
+    int g = multimap_module.gcd(x,y);
     System.out.println("The gcd of " + x + " and " + y + " is " + g);
     
     // Call the gcdmain() function
     String[] args = {"gcdmain","42","105"};
-    example.gcdmain(args);
+    multimap_module.gcdmain(args);
     
     // Call the count function
-    System.out.println(example.count("Hello World", 'l'));
+    System.out.println(multimap_module.count("Hello World", 'l'));
     
     // Call the capitalize function
     String[] capitalizeMe = {"hello world"};
-    example.capitalize(capitalizeMe);
+    multimap_module.capitalize(capitalizeMe);
     System.out.println(capitalizeMe[0]);
   }
 }
